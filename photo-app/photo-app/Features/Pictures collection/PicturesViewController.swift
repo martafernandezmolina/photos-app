d351131709db0c0544609f4017fbadaf97021cab
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-10-26 16:00:46 +0100

    añadida collectionView cells

---
+++ photo-app/photo-app/Features/Pictures collection/PicturesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  photo-app
+//
+//  Created by Marangi  on 26/10/2020.
+//
+
+import Foundation
+import UIKit
+class PicturesViewController:UIViewController{
+  
+  @IBOutlet weak var collectionView: PicturesViewController!
+  
+
+}
+
+extension PicturesViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 100
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellIdentifier", for: indexPath) // crea celdas reutilizadas
+     cell.backgroundColor = .black
+     return cell
+    
+  }
+  
+  
+  //esta clase no solo hereda del uiview si no qe tmb implementa el datasource -> es decir metodos del datasource mediante protocolo
+  
+
+  
+}
