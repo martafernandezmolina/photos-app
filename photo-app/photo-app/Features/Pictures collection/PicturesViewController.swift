4d4acefaee21792236d0a117cdbb9811a1e42010
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-10-28 17:51:09 +0100

    funcion width i height

--- photo-app/photo-app/Features/Pictures collection/PicturesViewController.swift
+++ photo-app/photo-app/Features/Pictures collection/PicturesViewController.swift
@@ -7,49 +7,79 @@
 
 import Foundation
 import UIKit
-class PicturesViewController:UIViewController{ // es un uiviewcontroller creado por nosotros.
-  
+class PicturesViewController: UIViewController { // es un uiviewcontroller creado por nosotros.
+  var cellWidth:CGFloat = 75   // valores por defecto al iniciar la aplicación.
+  var cellHeight:CGFloat = 75   // valores por defecto al iniciar la aplicación.
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var stackHorizontal: UIStackView!
+  @IBOutlet weak var year: UIButton!
+  @IBOutlet weak var day: UIButton!
+  @IBOutlet weak var month: UIButton!
+  @IBOutlet weak var allPhotos: UIButton!
   
   private let reuseIdentifier = String(describing: PictureCell.self)
   private let marginBetweenCells: CGFloat = 4.0
   
-  @IBAction func button1(_ sender: Any) {
-    print("boton1")
- 
-  }
-
   
-  @IBAction func button2(_ sender: Any) {
+  @IBAction func button1Touched(_ sender: UIButton) {
+    
+    selectionateActiveButton(sender)
+    cellWidth = 75
+    cellHeight = 75
+    collectionView.reloadData()
+    print("boton1")
+  }
+  
+  
+  @IBAction func button2Touched(_ sender: UIButton) {
+    selectionateActiveButton(sender)
+    cellWidth = 100
+    cellHeight = 100
+    collectionView.reloadData()
     print("boton2")
   }
   
-  @IBAction func button3(_ sender: Any) {
+  
+  @IBAction func button3Touched(_ sender: UIButton) {
+    selectionateActiveButton(sender)
+    cellWidth = 250
+    cellHeight = 250
+    collectionView.reloadData()
     print("boton3")
   }
   
-  @IBAction func button4(_ sender: Any) {
+  
+  @IBAction func button4Touched(_ sender: UIButton) {
+    selectionateActiveButton(sender)
+    cellWidth = 400
+    cellHeight = 400
+    collectionView.reloadData()
     print("boton4")
   }
   // reuseIdentifier devuelve el nombre de mi clse y crea PictureCell => es ifual a poner reuseIdentificer = PictureCell
   
+  
   override func viewDidLoad() {
+    super.viewDidLoad()
     
     stackHorizontal.layer.cornerRadius = 10
-     
-//
-//    func favorited(button: UIButton) {
-//
-//      button.isSelected = true
-//      button.backgroundColor = UIColor.redColor
-//      forState: .Selected)
-//
-//    }
-//
-//
+    
+//    year.isSelected = false
+//    day.isSelected = false
+//    month.isSelected = false
+//    allPhotos.isSelected = false
+    
    
-   
+    
+    // sender es el boton que he fet click
+    //
+    //    func favorited(button: UIButton) {
+    //
+    //      button.isSelected = true
+    //      button.backgroundColor =
+    //    }
+    
+    
     // viewdidload se llama siempre al cargar la pantalla. en nib guarda la vista del xib.
     // le dices que carge el xib en NIB (forma antigua) y le digo que use mis picturesCells como propias
     let nib = UINib(nibName: reuseIdentifier, bundle: nil)
@@ -60,7 +90,18 @@ class PicturesViewController:UIViewController{ // es un uiviewcontroller creado 
   }
   
   
+  func selectionateActiveButton(_ sender: UIButton) {
+    year.isSelected = false
+    day.isSelected = false
+    month.isSelected = false
+    allPhotos.isSelected = false
+    sender.isSelected = true
+  }
+  
 }
+
+
+// MARK: - extension PicturesViewController: UICollectionViewDataSource
 
 extension PicturesViewController: UICollectionViewDataSource {
   // delegado sobre el dataSource para que se entere de lo q pasa
@@ -69,26 +110,26 @@ extension PicturesViewController: UICollectionViewDataSource {
   
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
- 
+    
     return 100
     
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) // crea celdas reutilizadas
-     cell.backgroundColor = .black
-                            // confirmame que eres de tipo cell i guardate ahí
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) // crea celdas reutilizadas
+    cell.backgroundColor = .black
+    // confirmame que eres de tipo cell i guardate ahí
     if let pictureCell = (cell as? PictureCell){
-//      print(pictureCell.label.text)
-   //  pictureCell.label.text = String(indexPath.item)
+      //      print(pictureCell.label.text)
+      //  pictureCell.label.text = String(indexPath.item)
       //let image1 = UIImage(named:"pic2.jpg")
-     // pictureCell.imageView.image = UIImage(named: "pic2")
-     // if.row pom imagen pero si 
-    
+      // pictureCell.imageView.image = UIImage(named: "pic2")
+      // if.row pom imagen pero si
+      
       // pictureCell.label.text = indexPath.item
-       // pictureCell.label.text = indexPath.row
-     // print(indexPath.section)
+      // pictureCell.label.text = indexPath.row
+      // print(indexPath.section)
       
       if indexPath.row % 2  == 0 {
         
@@ -99,62 +140,67 @@ extension PicturesViewController: UICollectionViewDataSource {
         pictureCell.imageView.image = UIImage(named: "pic1")
         
       }
-    
       
-    //  pictureCell.imageView.image = indexPath.row % 2 == 0 ?  UIImage(named: "pic2"): UIImage(named: "pic1")
+      
+      //  pictureCell.imageView.image = indexPath.row % 2 == 0 ?  UIImage(named: "pic2"): UIImage(named: "pic1")
       //  pictureCell.imageView.image = indexPath.row % 2 == 0 ? imagen1 : imagen2
       
       // se puede hacer tmb con la funcion que devuelva  un bool true o false.
-          }
+    }
     
-     // crea/ devuelve Cells
+    // crea/ devuelve Cells
     // representacion visual celda super importnt
     // id que usa collectionview para saber que celdas se tiene q traer.
     
-     return cell
+    return cell
   }
-
+  
   //esta clase no solo hereda del uiview si no qe tmb implementa el datasource -> es decir metodos del datasource mediante protocolo
 }
-  extension PicturesViewController:UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-      print (indexPath)
-      print(indexPath.item)
-      print(indexPath.row)
-        //extension de la clase Pictureview que usa protocolo q implementa esta funcionalidad.
-       // escribir directamente didSelect
-      // super importante porq cuando pinchas tenemos q saber esa info.
-    }
-       
-  }
-    extension PicturesViewController:UICollectionViewDelegateFlowLayout {
-   // adapta visualmente la collectionView y hay que implementar metodos, que solo hay que bsuscarlo.
-      
-      func collectionView(_: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
-    
-         return CGSize.init(width:300, height:300)
-        
-      }
 
-      func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt: Int) -> CGFloat {
-        
-        return marginBetweenCells
-              
-    }
-      
-      func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumLineSpacingForSectionAt: Int) -> CGFloat {
-      
-        return marginBetweenCells
+
+// MARK: - extension PicturesViewController:UICollectionViewDelegate
+extension PicturesViewController:UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    print (indexPath)
+    print(indexPath.item)
+    print(indexPath.row)
+    //extension de la clase Pictureview que usa protocolo q implementa esta funcionalidad.
+    // escribir directamente didSelect
+    // super importante porq cuando pinchas tenemos q saber esa info.
+  }
+  
+}
+
+
+extension PicturesViewController:UICollectionViewDelegateFlowLayout {
+  // adapta visualmente la collectionView y hay que implementar metodos, que solo hay que bsuscarlo.
+  
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
     
-        
-      }
-      
-      
-      func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-        
-        
-      }
-        
-      
-        }
+    return CGSize.init(width: cellWidth, height: cellHeight)
+    
+  }
+  
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt: Int) -> CGFloat {
+    
+    return marginBetweenCells
+    
+  }
+  
+  
+  func collectionView(_: UICollectionView, layout: UICollectionViewLayout, minimumLineSpacingForSectionAt: Int) -> CGFloat {
+    
+    return marginBetweenCells
+    
+    
+  }
+  
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+    
+    
+  }
+  
+}
