c47b9f3e7ac3db5b2ec1ec09fa18fe33e72a14b1
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-10-29 15:36:34 +0100

    new class

---
+++ photo-app/photo-app/Features/Pictures collection/ImagesData.swift
@@ -0,0 +1,67 @@
+//
+//  ImagesData.swift
+//  photo-app
+//
+//  Created by Marangi  on 29/10/2020.
+//
+
+import Foundation
+import UIKit
+
+class ImagesData {
+    static func numOfImages() -> Int {
+//      let urls =  imagesUrl()?.count ?? 0
+//      return urls
+      
+//      if let url2:[URL] = imagesUrl() {
+//
+//        return url2.count
+//      }
+//      return 0
+ 
+      guard let urls:[URL] =  imagesUrl() else { return 0}
+      
+      return urls.count
+        
+    }
+    
+    static func imagesUrl() -> [URL]? {
+        return Bundle.main.urls(forResourcesWithExtension: "jpg", subdirectory: nil)
+      
+    }
+  
+  }
+  
+  
+     func imageForUrl(_ url: URL) -> UIImage? {    // aqui pasariamos un for in para pasar urls por un for in y luego me las da por parametro y lo convierto en uimage
+     
+        let imageName:String = url.lastPathComponent // te da el nombre del archivo
+       
+           return  UIImage(named: imageName)
+      }
+
+    }
+    
+    static func imageForPosition(_ position: Int) -> UIImage? {
+        let arrayIndex: Int = position % numOfImages()
+        if arrayIndex < numOfImages() {
+            if let url: URL = imagesUrl()?[arrayIndex] {
+                return imageForUrl(url)
+            }
+        }
+        return nil
+    }
+  
+  let filePath = Bundle.main.path(forResource: "imageName", ofType: "jpg"), let image = UIImage(contentsOfFile: filePath) {
+      imageView.contentMode = .scaleAspectFit
+      imageView.image = image
+  }
+
+  
+  
+  
+  
+  
+  
+  
+}
