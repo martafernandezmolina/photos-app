3cc37402c72b87e1a488095e14d387842486f58c
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-11-02 16:19:17 +0100

    timer.timer

--- photo-app/photo-app/Features/RandomPicture/RandomPictureViewController.swift
+++ photo-app/photo-app/Features/RandomPicture/RandomPictureViewController.swift
@@ -11,22 +11,44 @@ class RandomPictureViewController: UIViewController {
   
   
   @IBOutlet weak var randomImage: UIImageView!
+ var timer: Timer = Timer()
   
   override func viewDidLoad() {
      super.viewDidLoad()
-    
-    //randomImage.image = PicturesViewModel.selectedImage
   
-    //randomImage.image = UIImage (named: "pic1")
-//    let randomNumber = Int.random(in: 0..<ImagesData.numOfImages())
-//    let img: UIImage? = ImagesData.imageForPosition(randomNumber)
-//    imageView.image = img
+     refreshPicture()
     
-    getRandomPicture()
     
+    
+    //bloque forma parte param
+   // mirar ->  Timer.scheduledTimer
       
-    }
+      
+      
     
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    timer.invalidate() // este es x si acaso
+    timer = Timer.scheduledTimer(timeInterval: 5.0,
+                          target: self, // esta en mi clase pq la funcion que quire que se ejecute es la refreshpicture
+                          selector: #selector(refreshPicture) ,
+                          userInfo: nil,
+                          repeats: true) // repeticion para q se refresque
+  
+    print ("he entrado")
+    
+  }
+  
+ 
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    timer.invalidate()
+    print ("he salido")
+    
+  }
+ 
     
   func getRandomPicture() -> UIImage?{
     
@@ -37,4 +59,25 @@ class RandomPictureViewController: UIViewController {
     
   }
   
+  
+  @objc func refreshPicture() {   // imagen aleatoria = imgview
+    
+    UIView.transition(with: randomImage,
+                      
+                      duration: 0.5,
+                      
+                      options: .transitionCrossDissolve,
+                      
+                      animations: {
+                        
+                        self.randomImage.image = self.getRandomPicture()
+                        
+                      }, completion: { _ in
+                        
+                        print("Animation finished") // esto es cuando see acabe la animación, qué quieres que se ejecure. por ejemplo otro timer?
+                        
+                      })
+    
+  }
+          
 }
