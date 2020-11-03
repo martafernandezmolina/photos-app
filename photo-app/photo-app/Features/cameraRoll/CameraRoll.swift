65e638e7d25140c1025d699dd0f693bb46598fa6
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-11-03 12:23:32 +0100

    xzxczcxzcxz

--- photo-app/photo-app/Features/cameraRoll/CameraRoll.swift
+++ photo-app/photo-app/Features/cameraRoll/CameraRoll.swift
@@ -8,29 +8,42 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+import Photos
 // hacer eso que sube y baja
 //pantalla + boton, ver que funcione imagen de prueba
 
-class CameraRoll: UIViewController {
-
+class CameraRoll:UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  var imagePicker: UIImagePickerController = UIImagePickerController()
+  
   @IBOutlet weak var imageView: UIImageView!
   
   @IBAction func chooseFoto(_ sender: Any) {
-    
+    gallery()
     print("funciono")
+    
     
   }
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
+ 
     imageView.image = UIImage(named: "pic1")
-    
-    
+  
   }
 
+  //el image piker es es la pantalla.
+ 
+  func gallery() {
+    if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+                imagePicker.delegate = self
+                imagePicker.sourceType = .savedPhotosAlbum
+                imagePicker.allowsEditing = true
+                present(imagePicker, animated: true, completion: nil)
+
+            }
+     }
   
 }
  
